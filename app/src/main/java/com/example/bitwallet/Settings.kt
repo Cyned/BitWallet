3a8a50f0c4ed63c99a73f6e8b79df2144c6835a6
cyned <cynedenis@gmail.com>
2019-05-20 23:28:43 +0300

    change settings view

--- app/src/main/java/com/example/bitwallet/Settings.kt
+++ app/src/main/java/com/example/bitwallet/Settings.kt
@@ -2,6 +2,7 @@ package com.example.bitwallet
 
 import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -10,17 +11,26 @@ import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import org.w3c.dom.Text
 
 class Settings : Activity() {
 
+    private lateinit var sharedPref: SharedPreferences
     val mAuth = FirebaseAuth.getInstance()
     lateinit var mDatabase : DatabaseReference
+    private val internalMem = com.example.bitwallet.internalMem()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         mDatabase = FirebaseDatabase.getInstance().getReference("Users")
+        sharedPref = getSharedPreferences(internalMem.PREF_NAME, internalMem.PRIVATE_MODE)
+        val username: String = sharedPref.getString(internalMem.PREF_USER, "").toString()
+
+
+        val usernameView = findViewById<View>(R.id.username) as TextView
+        usernameView.text = username
 
         val homeBtn = findViewById<View>(R.id.home) as ImageView
         homeBtn.setOnClickListener(View.OnClickListener {
